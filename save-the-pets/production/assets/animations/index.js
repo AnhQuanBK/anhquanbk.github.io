1ffc6f3ce46bc5f3de1e961accf9047807ba522e
Anh Quan <anhquan971998@gmail.com>
2024-10-23 16:34:14 +0700

    lo

--- save-the-pets/production/assets/animations/index.js
+++ save-the-pets/production/assets/animations/index.js
@@ -1,1 +1,20 @@
-System.register("chunks:///animations.js",[],function(){return{execute:function(){}}});(function(n){n("virtual:///prerequisite-imports/animations","chunks:///animations.js")})(function(n,t){System.register(n,[t],function(n,t){return{setters:[function(t){var e={};for(var i in t)if("default"!==i&&"__esModule"!==i)e[i]=t[i];n(e)}],execute:function(){}}})});
+System.register("chunks:///animations.js",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/animations', 'chunks:///animations.js'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
