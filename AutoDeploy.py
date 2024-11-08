39508d5d949fed897bbd8dcc9b8c28325e272d90
Anh Quan <anhquan971998@gmail.com>
2024-11-08 11:02:57 +0700

    AutoDeploy: 02cfeab1e4d78d71

---
+++ AutoDeploy.py
@@ -0,0 +1,26 @@
+# Generate a random hash of 16 characters for the commit
+
+import hashlib
+import os
+import sys
+
+
+def get_hash():
+    hashString = hashlib.md5()
+    hashString.update(os.urandom(128))
+    return hashString.hexdigest()[:16]
+
+
+def main():
+    # Run cmd command git add --all
+    os.system("git add --all")
+
+    # Run cmd command git commit -m "AutoDeploy: <hash>"
+    hashString = get_hash()
+    os.system("git commit -m \"AutoDeploy: " + hashString + "\"")
+
+    # Run cmd command git push origin master
+    os.system("git push")
+
+
+main()
