39f3aa8ab018ad8644af6573b9bddb3630cad869
Anh Quan <anhquan@Anhs-Mac-mini.local>
2025-12-05 15:24:49 +0700

    Deploy tcr mask 3

--- tile-connect-remaster/mask/cocos-js/spine-BGFFnNyc.js
+++
@@ -1,2 +0,0 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-CC34fKUR.wasm")}}}));
-//# sourceMappingURL=spine-BGFFnNyc.js.map
--- tile-connect-remaster/mask/cocos-js/spine.js-Dyxd49gW.js
+++
@@ -1,2 +0,0 @@
-System.register([],(function(t){"use strict";return{execute:function(){t("default","")}}}));
-//# sourceMappingURL=spine.js-Dyxd49gW.js.map
